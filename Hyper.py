e84a0c7a1988bc5aa98331b196ee0535cb5925a3
aritra-2506 <35579668+aritra-2506@users.noreply.github.com>
2020-01-27 18:36:41 +0100

    Add files via upload

---
+++ Hyper.py
@@ -0,0 +1,65 @@
+import keras
+from keras import layers
+from keras import models
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.preprocessing.image import ImageDataGenerator
+from numpy import expand_dims
+import matplotlib.pyplot as plt
+from collections import Counter
+from sklearn.metrics import confusion_matrix
+import itertools
+import numpy as np
+from keras.optimizers import Adam, Nadam
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
+
+def create_model(optimizer='adam', activation='relu', dropout_rate=0.0):
+
+    model = models.Sequential()
+
+    model.add(layers.Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(64, (5, 5), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.AveragePooling2D((2, 2)))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(10, activation=activation))
+    model.add(layers.Dropout(rate=dropout_rate))
+    model.add(layers.Dense(10, activation='softmax'))
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
+
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype('float32') / 255
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+
+model = KerasClassifier(build_fn=create_model, verbose=0)
+
+batch_size = [100, 500, 1000]
+epochs = [5, 10, 20, 50]
+optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+dropout_rate = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+param_grid = dict(batch_size=batch_size, epochs=epochs, optimizer=optimizer, activation=activation, dropout_rate=dropout_rate)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+grid_result = grid.fit(train_images, train_labels)
+
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+plot_model(model, to_file='model.png')
+
